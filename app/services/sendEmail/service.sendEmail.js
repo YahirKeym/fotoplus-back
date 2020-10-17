bc7876183baea72931d39a30e4e88c7460d14c59
Yahir Keymurth <arzkaner@gmail.com>
2020-10-17 17:03:36 -0500

    First commit fotoplus back

---
+++ app/services/sendEmail/service.sendEmail.js
@@ -0,0 +1,55 @@
+exports = (module.exports = {});
+const nodemailer = require('nodemailer');
+const Template = require('./templatesEmail/confirmationEmail/confirmOrder.js');
+exports.sendEmail = ({name, lastname, country, addressExt, addresInt,city,apartament,postalCode,phone,email,informationPlus,items,typeDatos,DNI,Nombre,RUC,RazonSocial})=> new Promise((resolve,reject)=>{
+    let configuration = {
+        host: "mail.fotoplus.pe",
+        port: 587,
+        auth: {
+            user: "tienda@fotoplus.pe",
+            pass: "elpolob121"
+            // pass: "4224579"
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    }
+    
+    let Transporter = nodemailer.createTransport(configuration);
+    let htmlForUser = Template.header();
+    let htmlForAdmin = Template.header();
+    htmlForAdmin += Template.adminEmail(name, lastname,email,phone,country,addressExt,addresInt,city,apartament,postalCode,informationPlus,typeDatos,DNI,Nombre,RUC,RazonSocial);
+    htmlForUser += Template.userEmail(name, lastname)
+    let total = 0;
+    for (let index = 0; index < items.length; index++) {
+        htmlForAdmin += Template.product(items[index]);
+        htmlForUser += Template.product(items[index]);
+        total = total + items[index].quantity * items[index].price;
+    }
+    htmlForAdmin += Template.footerAdm(name, lastname,total);
+    htmlForUser += Template.footerUser(total);
+    let EmailOptionsUser = {
+        from: 'tienda@fotoplus.pe',
+        to: email,
+        subject: 'Gracias por su pedido!',
+        html: htmlForUser
+    };
+    let EmailOptionsAdm = {
+        from: 'tienda@fotoplus.pe',
+        to: 'tienda@fotoplus.pe',
+        subject: 'Pedido Nuevo!',
+        html: htmlForAdmin
+    };
+    Transporter.sendMail(EmailOptionsUser,(err,info)=>{
+        // console.log("error 1 ", err)
+        if(err) reject(err);
+        // console.log("informacion 1", info)
+        Transporter.sendMail(EmailOptionsAdm,(err,info)=>{
+            // console.log("error 2 ", err)
+            if(err) reject(err);
+            // console.log("informacion 2", info)
+            resolve(info);
+        })
+        resolve(info);
+    })
+})
